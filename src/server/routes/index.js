ab6b55f639925635871e058eb4d14efa10c098c5
Tosin <midastouchproductions@gmail.com>
2016-10-14 16:16:54 -0500

    create routes and views for signup login index and about

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,16 +3,8 @@ const router = express.Router();
 
 const indexController = require('../controllers/index');
 
-router.get('/', function (req, res, next) {
-  const renderObject = {};
-  renderObject.title = 'Welcome to Express!';
-  indexController.sum(1, 2, (error, results) => {
-    if (error) return next(error);
-    if (results) {
-      renderObject.sum = results;
-      res.render('index', renderObject);
-    }
-  });
+router.get('/login', function (req, res, next) {
+  res.render('login');
 });
 
 module.exports = router;
