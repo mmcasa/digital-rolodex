4498b99ed5701c58a591239aa6db697ce807bb70
Joseph Tongay <dirk.tongay@gmail.com>
2016-10-14 18:07:33 -0500

    all migrations tested and works

---
+++ src/server/db/migrations/20161014180452_create_contact_created_tag_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('contact_created_tag', function (table) {
+    table.increments()
+    table.integer('created_tag_id').unsigned().index().references('id').inTable('created_tags').onDelete('CASCADE')
+    table.integer('contact_id').unsigned().index().references('id').inTable('contacts').onDelete('CASCADE')
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('contact_created_tag')
+};
