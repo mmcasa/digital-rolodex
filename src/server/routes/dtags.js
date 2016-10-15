9a37289b941176bb71d5e7e9073e93e96eb392da
Tosin <midastouchproductions@gmail.com>
2016-10-14 19:29:32 -0500

    create dtags routes

--- src/server/routes/dtags.js
+++ src/server/routes/dtags.js
@@ -5,8 +5,18 @@ const router = express.Router();
 // create a controller if you need functions in here
 // const [page]Controller = require('../controllers/[page]');
 
-router.get('/tags', function (req, res, next) {
-  // redirect to dashboard
+router.post('/dtags/assign', function (req, res, next) {
+  // adds relation ship in the default tags and contacts table
+  res.redirect('/users/:user');
+});
+
+router.patch('/dtags/assign', function (req, res, next) {
+  // deletes relationship then fills in the new one
+  res.redirect('/users/:user');
+});
+
+router.delete('/dtags/assign', function (req, res, next) {
+  // deletes relationship
   res.redirect('/users/:user');
 });
 
