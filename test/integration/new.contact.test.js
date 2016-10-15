70f4fc909ec3b34e520f792b31cd23c03fb3b05f
Taylor King <dekkofilms@gmail.com>
2016-10-15 16:08:06 -0500

    test written for new contact page, and file form added

--- test/integration/new.contact.test.js
+++ test/integration/new.contact.test.js
@@ -8,38 +8,50 @@ chai.use(chaiHttp);
 const server = require('../../src/server/app');
 
 // Tests for new contact pages
+describe('THE NEW CONTACT PAGE', () => {
 
-// tests that make sure whats supposed to be on the page is on the page
-describe('***CONTENT***', () => {
-
-});
-
-// tests that links and redirects go where they are supposed to go
-describe('***NAVIGATION***', () => {
-
-  describe('IF LOGGED IN', () => {
+  // tests that make sure whats supposed to be on the page is on the page
+  describe('***CONTENT***', () => {
 
   });
 
-  describe('IF LOGGED OUT', () => {
+  // tests that links and redirects go where they are supposed to go
+  describe('***NAVIGATION***', () => {
+
+    describe('IF LOGGED IN', () => {
+
+      it('should go to the new contact form page', (done) => {
+          chai.request(server)
+              .get('/users/1/contacts/new')
+              .end((err, res) => {
+                should.not.exist(err);
+                done();
+              });
+      });
+
+    });
+
+    describe('IF LOGGED OUT', () => {
+
+    });
 
   });
 
-});
-
-// tests that things look the way they are supposed to
-describe('***VIEWS***', () => {
-
-});
-
-// test that only the right people are in the right places
-describe('***AUTH***', () => {
-
-  describe('IF LOGGED IN', () => {
+  // tests that things look the way they are supposed to
+  describe('***VIEWS***', () => {
 
   });
 
-  describe('IF LOGGED OUT', () => {
+  // test that only the right people are in the right places
+  describe('***AUTH***', () => {
+
+    describe('IF LOGGED IN', () => {
+
+    });
+
+    describe('IF LOGGED OUT', () => {
+
+    });
 
   });
 
