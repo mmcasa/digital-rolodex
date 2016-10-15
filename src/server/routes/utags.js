235849cc5f6b9b0e6e63ad34c11cf26db99826e4
Tosin <midastouchproductions@gmail.com>
2016-10-14 19:23:16 -0500

    scaffolding

--- src/server/routes/utags.js
+++ src/server/routes/utags.js
@@ -5,14 +5,9 @@ const router = express.Router();
 // create a controller if you need functions in here
 // const [page]Controller = require('../controllers/[page]');
 
-router.get('/users', function (req, res, next) {
+router.get('/tags', function (req, res, next) {
   // redirect to dashboard
   res.redirect('/users/:user');
-});
-
-router.get('/users/:user', function (req, res, next) {
-  // dashboard
-  res.render('dashboard');
 });
 
 
