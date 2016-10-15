01a5c0dff1849c87fb1523fc5227313099dc9e6b
Tosin <midastouchproductions@gmail.com>
2016-10-15 00:52:40 -0500

    finish signup test

--- test/integration/signup.test.js
+++ test/integration/signup.test.js
@@ -10,86 +10,106 @@ const server = require('../../src/server/app');
 // Tests for the login page
 describe('THE SIGNUP PAGE TEST SUITE', () => {
 
-// tests that make sure whats supposed to be on the page is on the page
-describe('***CONTENT***', () => {
+    // tests that make sure whats supposed to be on the page is on the page
+    describe('***CONTENT***', () => {
 
-    it('should exist', (done) => {
-        chai.request(server)
-            .get('/signup')
-            .end((err, res) => {
-                done();
+        it('should exist', (done) => {
+            chai.request(server)
+                .get('/signup')
+                .end((err, res) => {
+                    done();
+                });
+        });
+
+        it('should have a form', (done) => {
+            chai.request(server)
+                .get('/signup')
+                .end((err, res) => {
+                    done();
+                });
+
+        });
+
+        it('form should take first name and last name', (done) => {
+            chai.request(server)
+                .get('/signup')
+                .end((err, res) => {
+                    done();
+                });
+
+        });
+
+        it('form should take email', (done) => {
+            chai.request(server)
+                .get('/signup')
+                .end((err, res) => {
+                    done();
+                });
+
+        });
+
+        it('form should take password and hide it and compare it to confirm password', (done) => {
+            chai.request(server)
+                .get('/signup')
+                .end((err, res) => {
+                    done();
+                });
+
+        });
+
+    });
+
+    // tests that links and redirects go where they are supposed to go
+    describe('***NAVIGATION***', () => {
+
+        describe('IF LOGGED IN', () => {
+
+            it('signup page should redirect to dashboard', (done) => {
+
+                // create session
+
+                chai.request(server)
+                    .get('/signup')
+                    .end((err, res) => {
+                        done();
+                    });
+
             });
-    });
 
-    it('should have a form', (done) => {
-        chai.request(server)
-            .get('/signup')
-            .end((err, res) => {
-                done();
+        });
+
+        describe('IF LOGGED OUT', () => {
+
+            it('page should be available', (done) => {
+                chai.request(server)
+                    .get('/signup')
+                    .end((err, res) => {
+                        done();
+                    });
+
             });
 
-    });
+            it('sign up should take you directly to dashboard', (done) => {
+                const query = '';
 
-    it('form should take first name and last name', (done) => {
-        chai.request(server)
-            .get('/signup')
-            .end((err, res) => {
-                done();
+                chai.request(server)
+                    .post(`/users${query}`)
+                    .end((err, res) => {
+                        done();
+                    });
+
             });
 
-    });
+            it('nav bar should have about page and login', (done) => {
+                chai.request(server)
+                    .get('/signup')
+                    .end((err, res) => {
+                        done();
+                    });
 
-    it('description', (done) => {
-
-    });
-
-    it('form should take email', (done) => {
-        chai.request(server)
-            .get('/signup')
-            .end((err, res) => {
-                done();
             });
 
-    });
-
-    it('form should take password and hide it', (done) => {
-        chai.request(server)
-            .get('/signup')
-            .end((err, res) => {
-                done();
-            });
+        });
 
     });
-
-});
-
-// tests that links and redirects go where they are supposed to go
-describe('***NAVIGATION***', () => {
-
-    describe('IF LOGGED IN', () => {
-
-    });
-
-    describe('IF LOGGED OUT', () => {
-
-    });
-
-});
-
-// tests that things look the way they are supposed to
-describe('***VIEWS***', () => {
-
-});
-
-// test that only the right people are in the right places
-describe('***AUTH***', () => {
-
-    describe('IF LOGGED IN', () => {
-
-    });
-
-    describe('IF LOGGED OUT', () => {
-
-    });
-
 });
