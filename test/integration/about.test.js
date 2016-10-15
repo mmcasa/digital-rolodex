1a6b95ecff61e4ea8576194b5d4f9145a01992b7
Tosin <midastouchproductions@gmail.com>
2016-10-14 22:17:58 -0500

    base outline complete

--- test/integration/about.test.js
+++ test/integration/about.test.js
@@ -8,23 +8,90 @@ chai.use(chaiHttp);
 const server = require('../../src/server/app');
 
 // Tests for the about page
+describe('THE ABOUT PAGE TEST SUITE', () => {
 
-// tests that make sure whats supposed to be on the page is on the page
-describe('***CONTENT***', () => {
+    // tests that make sure whats supposed to be on the page is on the page
+    describe('***CONTENT***', () => {
 
-});
+        it('should exist', (done) => {
+            chai.request(server)
+                .get('/about')
+                .end((err, res) => {
+                  done();
+                });
+        });
 
-// tests that links and redirects go where they are supposed to go
-describe('***NAVIGATION***', () => {
+        it('should have information about the service', (done) => {
+          chai.request(server)
+              .get('/about')
+              .end((err, res) => {
+                  done();
+              });
+        });
 
-});
+    });
 
-// tests that things look the way they are supposed to
-describe('***VIEWS***', () => {
+    // tests that links and redirects go where they are supposed to go
+    describe('***NAVIGATION***', () => {
 
-});
+        describe('IF LOGGED IN', () => {
 
-// test that only the right people are in the right places
-describe('***AUTH***', () => {
+            it('should have a link to dashboard', (done) => {
+              chai.request(server)
+                  .get('/about')
+                  .end((err, res) => {
+                      done();
+                  });
+            });
+
+        });
+
+        describe('IF LOGGED OUT', () => {
+
+            it('should have a link to login', (done) => {
+              chai.request(server)
+                  .get('/about')
+                  .end((err, res) => {
+                      done();
+                  });
+            });
+
+            it('should have a link to sign up', (done) => {
+              chai.request(server)
+                  .get('/about')
+                  .end((err, res) => {
+                      done();
+                  });
+            });
+
+        });
+
+    });
+
+    // tests that things look the way they are supposed to
+    describe('***VIEWS***', () => {
+
+        it('navbar at the top', (done) => {
+          chai.request(server)
+              .get('/about')
+              .end((err, res) => {
+                  done();
+              });
+        });
+
+    });
+
+    // test that only the right people are in the right places
+    describe('***AUTH***', () => {
+
+      describe('IF LOGGED IN', () => {
+
+      });
+
+      describe('IF LOGGED OUT', () => {
+
+      });
+
+    });
 
 });
--- test/integration/utags.test.js
+++ test/integration/utags.test.js
@@ -17,6 +17,14 @@ describe('***CONTENT***', () => {
 // tests that links and redirects go where they are supposed to go
 describe('***NAVIGATION***', () => {
 
+  describe('IF LOGGED IN', () => {
+
+  });
+
+  describe('IF LOGGED OUT', () => {
+
+  });
+
 });
 
 // tests that things look the way they are supposed to
@@ -26,5 +34,13 @@ describe('***VIEWS***', () => {
 
 // test that only the right people are in the right places
 describe('***AUTH***', () => {
+
+  describe('IF LOGGED IN', () => {
+
+  });
+
+  describe('IF LOGGED OUT', () => {
+
+  });
 
 });
