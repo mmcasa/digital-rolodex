9a794ff7f3908981be5f61d3d516dce671aca347
Tosin <midastouchproductions@gmail.com>
2016-10-14 22:05:22 -0500

    new tests

--- test/integration/contacts.test.js
+++ test/integration/contacts.test.js
@@ -7,4 +7,24 @@ chai.use(chaiHttp);
 
 const server = require('../../src/server/app');
 
-// Tests for the company page
+// Tests for the contacts pages
+
+// tests that make sure whats supposed to be on the page is on the page
+describe('***CONTENT***', () => {
+
+});
+
+// tests that links and redirects go where they are supposed to go
+describe('***NAVIGATION***', () => {
+
+});
+
+// tests that things look the way they are supposed to
+describe('***VIEWS***', () => {
+
+});
+
+// test that only the right people are in the right places
+describe('***AUTH***', () => {
+
+});
