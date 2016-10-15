48d96199d6c01b7eb6cfff76c5f1df3f0f6ff137
Tosin <midastouchproductions@gmail.com>
2016-10-15 00:44:01 -0500

    login and sign up test

--- test/integration/signup.test.js
+++ test/integration/signup.test.js
@@ -8,23 +8,71 @@ chai.use(chaiHttp);
 const server = require('../../src/server/app');
 
 // Tests for the login page
+describe('THE SIGNUP PAGE TEST SUITE', () => {
 
 // tests that make sure whats supposed to be on the page is on the page
 describe('***CONTENT***', () => {
 
+    it('should exist', (done) => {
+        chai.request(server)
+            .get('/signup')
+            .end((err, res) => {
+                done();
+            });
+    });
+
+    it('should have a form', (done) => {
+        chai.request(server)
+            .get('/signup')
+            .end((err, res) => {
+                done();
+            });
+
+    });
+
+    it('form should take first name and last name', (done) => {
+        chai.request(server)
+            .get('/signup')
+            .end((err, res) => {
+                done();
+            });
+
+    });
+
+    it('description', (done) => {
+
+    });
+
+    it('form should take email', (done) => {
+        chai.request(server)
+            .get('/signup')
+            .end((err, res) => {
+                done();
+            });
+
+    });
+
+    it('form should take password and hide it', (done) => {
+        chai.request(server)
+            .get('/signup')
+            .end((err, res) => {
+                done();
+            });
+
+    });
 
 });
 
 // tests that links and redirects go where they are supposed to go
 describe('***NAVIGATION***', () => {
 
-  describe('IF LOGGED IN', () => {
+    describe('IF LOGGED IN', () => {
 
-  });
+    });
 
-  describe('IF LOGGED OUT', () => {
+    describe('IF LOGGED OUT', () => {
 
-  });
+    });
 
 });
 
@@ -36,12 +84,12 @@ describe('***VIEWS***', () => {
 // test that only the right people are in the right places
 describe('***AUTH***', () => {
 
-  describe('IF LOGGED IN', () => {
+    describe('IF LOGGED IN', () => {
 
-  });
+    });
 
-  describe('IF LOGGED OUT', () => {
+    describe('IF LOGGED OUT', () => {
 
-  });
+    });
 
 });
